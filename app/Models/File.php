d8ff856a066714b77a6c8b722965cc742433f2b7
Ahmed M. Elhalaby <Eng.Ahmed.Elhalaby@gmail.com>
2022-02-20 14:37:37 +0200

    .

--- app/Models/File.php
+++ app/Models/File.php
@@ -2,9 +2,9 @@
 
 namespace App\Models;
 
+use ElasticScoutDriverPlus\Searchable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Laravel\Scout\Searchable;
 
 class File extends Model
 {
